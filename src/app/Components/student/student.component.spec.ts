1eea33fa8ba0a7655cfe0fd0accacb7b288db5bf
markj <sc.markj0307@gmail.com>
2024-04-10 01:42:15 +0800

    Push test cases with Spyon to Mock and Stub methods

--- src/app/Components/student/student.component.spec.ts
+++ src/app/Components/student/student.component.spec.ts
@@ -3,6 +3,8 @@ import { StudentComponent } from './student.component';
 import { StudentService } from 'src/app/Services/student.service';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { of } from 'rxjs';
 
 describe('StudentComponent', () => {
   let component: StudentComponent;
@@ -25,7 +27,7 @@ describe('StudentComponent', () => {
 
 
 
-  //check if the saveData method call the calculate method
+  //check if the saveData method call the calculate method by mocking the calculate method.
   it('SpyOn method',()=>{
     spyOn(component,'calculate')
     component.saveData();
@@ -33,8 +35,27 @@ describe('StudentComponent', () => {
   })
 
 
-
+//mocking the return value of the calculate method in order to check if the StudentResult function is working properly
   it('SpyOn method with return value',()=>{
-    spyOn(component,'calculate')
+    spyOn(component,'calculate').and.returnValue(41);
+    let result = component.StudentResult();
+    expect(result).toEqual("Pass");
+  })
+
+
+
+  //spyon servicec
+  it('',()=>{
+    let service = fixture.debugElement.injector.get(StudentService);
+    spyOn(service,"SaveDetails").and.callFake(()=>{
+        return of({
+          "result1":200
+        })
+    });
+
+    component.saveData();
+    expect(component.result).toEqual({
+      "result1":200
+    })
   })
 });
